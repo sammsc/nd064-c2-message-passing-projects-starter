6a739cf49dde49cf50a9db68e19ca43ffde7d072
Sam Chien <chiensam@gmail.com>
2022-12-14 23:03:22 +0100

    rest api done

--- modules/api_rest/app/udaconnect/services.py
+++ modules/api_rest/app/udaconnect/services.py
@@ -4,7 +4,8 @@ from typing import Dict, List
 
 from app import db
 from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
+# from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
+# from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
 
 logging.basicConfig(level=logging.WARNING)
